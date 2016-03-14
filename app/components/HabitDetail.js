1be78a76aa6066a451cba0ac5ca7b313ef7aed7e
Dan Jensen <dj@fitbird.com>
2016-03-14 07:54:29 -0400

    update styling

--- app/components/HabitDetail.js
+++ app/components/HabitDetail.js
@@ -8,40 +8,41 @@ export default class HabitDetail extends Component {
     habit: PropTypes.object.isRequired,
     actions: PropTypes.object.isRequired
   };
-  render = () => {
+  render() {
     const { habit } = this.props
     return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerText}>{habit.nameLong}</Text>
-      </View>
-      <View style={styles.descriptionContainer}>
-        <Text style={styles.descriptionText}>{habit.description}</Text>
-      </View>
-      <View>
-        {(habit.selected)
-          ?
-          <Button onPress={() => this.props.actions.unselectHabit(habit)}>
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerText}>{habit.nameLong}</Text>
+        </View>
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.descriptionText}>{habit.description}</Text>
+        </View>
+        <View>
+          {(habit.selected)
+            ?
+            <Button onPress={() => this.props.actions.unselectHabit(habit)}>
+              <View style={styles.buttonContainer}>
+                <Text style={[ styles.buttonText, styles.unselect ]}>UNSELECT</Text>
+              </View>
+            </Button>
+            :
+            <Button onPress={() => this.props.actions.selectHabit(habit)}>
+              <View style={[ styles.buttonContainer, styles.select ]}>
+                <Text style={styles.buttonText}>SELECT</Text>
+              </View>
+            </Button>
+          }
+        </View>
+        <View>
+          <Button onPress={() => this.onBack}>
             <View style={styles.buttonContainer}>
-              <Text style={[ styles.buttonText, styles.unselect ]}>UNSELECT</Text>
+              <Text style={styles.buttonText}>BACK</Text>
             </View>
           </Button>
-          :
-          <Button onPress={() => this.props.actions.selectHabit(habit)}>
-            <View style={[ styles.buttonContainer, styles.select ]}>
-              <Text style={styles.buttonText}>SELECT</Text>
-            </View>
-          </Button>
-        }
+        </View>
       </View>
-      <View>
-        <Button onPress={() => this.onBack}>
-          <View style={styles.buttonContainer}>
-            <Text style={styles.buttonText}>BACK</Text>
-          </View>
-        </Button>
-      </View>
-    </View>
-  )};
-  onBack = () => this.props.nav.pop();
+    )
+  }
+  onBack() { this.props.nav.pop() }
 }
--- app/components/SectionHeader.js
+++ app/components/SectionHeader.js
@@ -2,7 +2,7 @@ import React, { Component, Text, View } from 'react-native'
 import styles from './SectionHeaderStyles'
 
 export default class SectionHeader extends Component {
-  render = () => {
+  render() {
     const header = 'It’s time to choose a habit'
     const subheader = 'Good habits are the key to long term success regardless what your goal is. Pick one of the habits below to do daily for the next two weeks. Start with something you’re certain you can do and build on it.'
     return (
@@ -15,5 +15,5 @@ export default class SectionHeader extends Component {
         </View>
       </View>
     )
-  };
+  }
 }
