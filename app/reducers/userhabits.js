b443e9679a2498899a61c694e4ef3b4bed0d3300
Dan Jensen <dj@fitbird.com>
2016-03-21 18:22:04 -0400

    update userhabit reducer

--- app/reducers/userhabits.js
+++ app/reducers/userhabits.js
@@ -1,22 +1,21 @@
-function deleteUserhabit(state, _id) {
-  const newState = Object.assign({}, state)
-  delete newState[_id]
-  return newState
-}
-
 export default function userhabits(state = {}, action = {}) {
   switch (action.type) {
     case 'GET_USERHABITS':
       return Object.assign({}, state,
-        action.userhabits.reduce((obj, userhabit) => {
-          obj[userhabit._id] = userhabit
-          return obj
+        action.userhabits.reduce((result, userhabit) => {
+          result[userhabit._id] = userhabit
+          return result
         }, {})
       )
     case 'CREATE_USERHABIT':
       return { ...state, [action.userhabit._id]: action.userhabit }
     case 'DELETE_USERHABIT':
-      return deleteUserhabit(state, action.userhabit._id)
+      return Object.assign({},
+        Object.keys(state).reduce((result, k) => {
+          if (k !== action.userhabit._id) { result[k] = state[k] }
+          return result
+        }, {})
+      )
     default:
       return state
   }
